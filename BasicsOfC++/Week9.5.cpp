8c89bd9d8bd29033f2a5a584ce6d6a58f45ec8f4
piyush <piyush.khadka2233@gmail.com>
2025-09-27 17:42:05 +0100

    singly linked list

---
+++ BasicsOfC++/Week9.5.cpp
@@ -0,0 +1,222 @@
+﻿//🔹 Mini Project 1: Singly Linked List — Student Record Manager
+//
+//Goal : Store student names in a linked list.Add, display, and delete by name.
+//
+//Features
+//
+//Add a new student(at the end).
+//
+//Display all students.
+//
+//Delete a student by name.
+
+
+
+//#include<iostream>
+//
+//struct Node {
+//	std::string data;
+//	Node* next;
+//};
+//
+//void display(Node* temp) {
+//	while (temp != nullptr) {
+//		std::cout << temp->data << " " << std::endl;
+//		std::cout << "----------------"<< std::endl;
+//		temp = temp->next;
+//	}
+//}
+//void add(Node*& head, std::string& name) {
+//	Node* temp = head;
+//	Node* newNode = new Node{ name , nullptr };
+//
+//	while (temp->next != nullptr) { // until temp is the last next value as it needs to point to the new node
+//		temp = temp->next;
+//	}
+//	temp->next = newNode; // temp.next == newNode
+//	std::cout << "added to the memory" << std::endl;
+//}
+//
+//void deleted(Node*& head, std::string& name) {
+//
+//	if (head == nullptr) {
+//		return;
+//	}
+//
+//	if (head->data == name) {
+//		Node* temp = head;
+//		head = head->next;
+//		delete temp;
+//	}
+//
+//	Node* prev = head;
+//	Node* curr = head->next;
+//
+//
+//	while (curr != nullptr && curr->data != name ){
+//		prev = curr;
+//		curr = curr->next;
+//	}
+//
+//	if (curr != nullptr) {
+//		prev->next = curr->next; // joining the previous next to current before  deleted next --> beautiful
+//		delete curr;
+//	}
+//}
+//int main() {
+//
+//	Node* head;
+//	head = new Node{ "Piyush", nullptr };
+//	head->next = new Node{ "Oana", nullptr };
+//	head->next->next = new Node{ "Rachel", nullptr };
+//
+//	int record;
+//	std::string name;
+//	bool running = true;
+//	std::string fired;
+//
+//	while (running) {
+//		std::cout << "1. Add \n2. Display \n3. Delete.\n4.Exit \n";
+//		std::cout << "Do you want to add/ display or delete student record? " << std::endl;
+//		std::cin >> record;
+//		switch (record) {
+//		case 1:
+//			system("cls");
+//			std::cout << "Enter the name of the student" << std::endl;
+//			std::cin >> name;
+//			add(head, name);
+//			break;
+//
+//		case 2:
+//			system("cls");
+//			display(head);
+//			break;
+//
+//		case 3:
+//			system("cls");
+//			std::cout << "Enter the name of the one to be fired!!" << std::endl;
+//			std::cin >> fired;
+//			deleted(head, fired);
+//			break;
+//
+//		case 4:
+//			running = false;
+//			break;
+//
+//		default:
+//			std::cout << "Invalid Choices!";
+//		}
+//	}
+//		
+//
+//	return 0;
+//
+//}
+
+#include<iostream>
+
+
+
+struct Node {
+	std::string data;
+	Node* next;
+};
+
+//void add(std::string& name, Node*& head) {
+//	Node* temp = head;
+//	Node* newNode = new Node{ name, nullptr };
+//
+//	if (head == nullptr) {
+//		return;
+//	}
+//
+//	while (temp->next != nullptr) {
+//		temp = temp->next;
+//	}
+//	temp->next = newNode;
+//};
+//
+//void Delete(std::string& name, Node* head) {
+//	
+//	if (head == nullptr) {
+//		return;
+//	}
+//
+//	if (head->data == name) {
+//		Node* temp = head;
+//		head = head->next;
+//		delete temp;
+//	}
+//
+//	Node* prev = head;
+//	Node* curr = head->next;
+//
+//	while (curr->next != nullptr && curr->data != name) {
+//		prev = curr;
+//		curr = curr->next;
+//	}
+//
+//	if (curr != nullptr) {
+//		prev->next = curr->next;
+//		delete curr;
+//	}
+//
+//}
+
+//int main() {
+//	Node* head;
+//	head = new Node{ "piyush", nullptr };
+//	head->next = new Node{ "Oana", nullptr };
+//	head->next->next = new Node{ "Rachel", nullptr };
+//
+//	Node* prev = nullptr;
+//	Node* curr = head;
+//	Node* next = nullptr;
+//
+//	while (curr != nullptr) {
+//		next = curr->next;
+//		curr->next = prev;
+//		prev = curr;
+//		curr = next;
+//	}
+//
+//	while (prev != nullptr) {
+//		std::cout << prev->data;
+//		prev = prev->next;
+//	}
+
+	/*std::string ask;
+	std::cout << "add at start of the node" << std::endl;
+	std::cin >> ask;
+
+	Node* temp = new Node{ ask, nullptr };
+	temp->next = head;
+
+	std::cout << temp->data;
+
+	while (temp != nullptr) {
+		std::cout << temp->data;
+		temp = temp->next;
+	}*/
+	
+
+	/*std::string record;
+	std::string name;*/
+	/*std::cout << "Choose" << std::endl;
+	std::cin >> record;*/
+
+	/*if (record == "1") {
+		std::cout << "Enter the name" << std::endl;
+		std::cin >> name;
+		add(name, head);
+	}
+	else if (record == "2") {
+		std::cout << "Enter the name" << std::endl;
+		std::cin >> name;
+		Delete(name , head);
+	}*/
+
+	
+
+}
+
