edef4db8669608924a39a1af609fa3cee0dc53c3
piyush <piyush.khadka2233@gmail.com>
2025-10-19 21:47:10 +0100

    Binary Search Tree

--- BasicsOfC++/Week9.9.cpp
+++ BasicsOfC++/Week9.9.cpp
@@ -10,15 +10,28 @@ struct Node {
 	Node* right;
 };
 
+Node* newNode(int data) {
+	Node* newNode = new Node{data, nullptr, nullptr};
+	return newNode;
+}
 
-void add(Node*& root) {
+void add(Node*& root, int data) {
+	if (root == nullptr) {
+		root = new Node{ data, nullptr, nullptr };
+	}
+	if (data < root->data)
+		add(root->left, data);
+	else if (data > root->data)
+		add(root->right, data);
 
 }
 
 
 int main() {
-	Node* root = new Node{ 10, nullptr };
+	Node* root = nullptr;
 
-	add(root);
+	add(root, 10);
+	add(root, 20);
+	add(root, 15);
 	return 0;
 }
