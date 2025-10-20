460d1eb21a2430cc61d6c3d0083629d5a4b9e9af
piyush <piyush.khadka2233@gmail.com>
2025-10-20 19:51:09 +0100

    Binary Search Tree -> search recursion

--- BasicsOfC++/Week9.9.cpp
+++ BasicsOfC++/Week9.9.cpp
@@ -26,12 +26,25 @@ void add(Node*& root, int data) {
 
 }
 
+bool search(Node*& root, int data) {
+	if (root == nullptr) return false;
+	else if (root->data == data) return true;
+	else if (data < root->data) return search(root->left, data);
+	else if (data > root->data) return search(root->right, data);
+}
+
 
 int main() {
 	Node* root = nullptr;
-
+	int num;
+	
 	add(root, 10);
 	add(root, 20);
 	add(root, 15);
+	std::cout << "Enter a number" << std::endl;
+	std::cin >> num;
+
+	if (search(root, num) == true) std::cout << "found!" << std::endl;
+	else std::cout << "Not found!" << std::endl;
 	return 0;
 }
