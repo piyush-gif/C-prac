05404aa5b425b16916dc971f083bd6e06b085fc3
piyush <piyush.khadka2233@gmail.com>
2025-10-25 22:18:10 +0100

    graph  -> dfs

--- BasicsOfC++/Week9.10.cpp
+++ BasicsOfC++/Week9.10.cpp
@@ -31,10 +31,20 @@
 #include<vector>
 #include<algorithm>
 
+void dfs(std::vector<bool>& visited, std::vector<std::vector<int>>& graph, int node) {
+	visited[node] = true;
+	std::cout << char('A' + node) << " ";
+
+	for (int next : graph[node]) {
+		if (!visited[next]) {
+			dfs(visited, graph, next);
+		}
+	}
+}
 
 
 int main() {
-	int n = 4;
+	int n = 6;
 	std::vector<std::vector<int>> graph(n);
 
 	graph[0].push_back(1);
@@ -50,6 +60,10 @@ int main() {
 
 	graph[3].push_back(2);
 	graph[3].push_back(0);
+
+	std::vector<bool> visited(n, false);
+
+	dfs(visited, graph, 0);
 }
 
 
