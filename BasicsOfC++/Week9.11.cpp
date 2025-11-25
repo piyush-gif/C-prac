0c28a3fccbbf5400a6ca620ba67e25fe52f2f51b
piyush <piyush.khadka2233@gmail.com>
2025-11-25 18:01:41 +0000

    bubble sort

--- BasicsOfC++/Week9.11.cpp
+++ BasicsOfC++/Week9.11.cpp
@@ -1,32 +1,32 @@
 ﻿// classss
 
-#include<iostream>
-
-class Vec2 {
-public:
-	int x, y;
-	Vec2(int a, int b) {
-		x = a;
-		y = b;
-	}
-
-	Vec2 add(Vec2 a) {
-		x += a.x;
-		y += a.y;
-		return *this;
-	}
-	void print() {
-		std::cout << "x : " << x << " y : " << y;
-	}
-
-};
-
-
-
-int main() {
-	Vec2 vec(2, 3);
-	Vec2 vec1(3, 4);
-	Vec2 vec2(5, 6);
-	vec.add(vec1).print();
-
-}
+//#include<iostream>
+//
+//class Vec2 {
+//public:
+//	int x, y;
+//	Vec2(int a, int b) {
+//		x = a;
+//		y = b;
+//	}
+//
+//	Vec2 add(Vec2 a) {
+//		x += a.x;
+//		y += a.y;
+//		return *this;
+//	}
+//	void print() {
+//		std::cout << "x : " << x << " y : " << y;
+//	}
+//
+//};
+//
+//
+//
+//int main() {
+//	Vec2 vec(2, 3);
+//	Vec2 vec1(3, 4);
+//	Vec2 vec2(5, 6);
+//	vec.add(vec1).print();
+//
+//}
