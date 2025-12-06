480acb43500cad520823bf2d4fd8cf67beabf577
piyush <piyush.khadka2233@gmail.com>
2025-12-06 15:19:43 +0000

    classes

--- BasicsOfC++/Class.cpp
+++ BasicsOfC++/Class.cpp
@@ -1,66 +1,85 @@
-﻿#include<iostream>
-#include<vector>
-
-class Matrix{
-public:
-	std::vector<std::vector<int>> mat;
-
-	void add(Matrix* mat) {
-
-	}
-	
-};
-
-std::vector<std::vector<int>> addMatrix(std::vector<std::vector<int>> mat1, std::vector<std::vector<int>> mat2) {
-	std::vector<std::vector<int>> mat3;
-	for (int i = 0; i < mat1.size(); i++) {
-		std::vector<int> tot;
-		for (int j = 0; j < mat1.size(); j++) {
-			int total = 0;
-			for (int k = 0; k < mat2.size(); k++) {
-				total += mat1[i][k] + mat2[k][j];
-				
-			}
-			tot.push_back(total);
-		}
-		mat3.push_back(tot);
-	}
-	return mat3;
-}
-
-std::vector<std::vector<int>> scaleMatrix(std::vector<std::vector<int>> mat3, int scale) {
-	std::vector<std::vector<int>> scaled;
-	for (int i = 0; i < mat3.size(); i++) {
-		std::vector<int> scaling;
-		for (int j = 0; j < mat3[i].size(); j++) {
-			scaling.push_back(mat3[i][j] * scale);
-		}
-		scaled.push_back(scaling);
-	}
-	return scaled;
-}
-
-void printMatrix(std::vector<std::vector<int>> mat3) {
-	for (int i = 0; i < mat3.size(); i++) {
-		for (int j = 0; j < mat3[i].size(); j++) {
-			std::cout << mat3[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-}
-
-
-
-int main() {
-	/*Matrix* mat = new Matrix();
-	mat->add(mat);*/
-	std::vector<std::vector<int>> mat1 = { {1, 2, 3},
-										   {2, 3, 4} };
-	std::vector<std::vector<int>> mat2 = { {1, 2},
-										   {2, 3},
-										   {4, 5} };
-
-	std::vector<std::vector<int>> vec = addMatrix(mat1, mat2);
-	printMatrix(vec);
-	printMatrix(scaleMatrix(vec, 3));
-}
+﻿//#include<iostream>
+//#include<vector>
+//using namespace std;
+//class Matrix {
+//public:
+//	vector<vector<int>> mat;
+//
+//	Matrix(vector<vector<int>> matrix) {
+//		this->mat = matrix;
+//	}
+//	Matrix* addMatrix(Matrix* mat2) {
+//		for (int i = 0; i < this->mat.size(); i++) {
+//			for (int j = 0; j < this->mat[i].size(); j++) {
+//				this->mat[i][j] += mat2->mat[i][j];
+//			}
+//		}
+//		return this;
+//	}
+//
+//	Matrix* subMatrix(Matrix* mat2) {
+//		for (int i = 0; i < this->mat.size(); i++) {
+//			for (int j = 0; j < this->mat[i].size(); j++) {
+//				this->mat[i][j] -= mat2->mat[i][j];
+//			}
+//		}
+//		return this;
+//	}
+//
+//	Matrix* scaling(int scale) {
+//		for (int i = 0; i < this->mat.size(); i++) {
+//			for (int j = 0; j < this->mat[i].size(); j++) {
+//				this->mat[i][j] *= scale;
+//			}
+//		}
+//		return this;
+//	}
+//
+//	Matrix* mulMatrix(Matrix* mat2) {
+//		vector<vector<int>> vec;
+//		for (int i = 0; i < this->mat.size(); i++) {
+//			vector<int> vec1;
+//			for (int j = 0; j < this->mat.size(); j++) {
+//				int total = 0;
+//				for (int k = 0; k < mat2->mat.size(); k++) {
+//					
+//					total += this->mat[i][k] * mat2->mat[k][j];
+//					
+//				}
+//				vec1.push_back(total);
+//				
+//			}
+//			vec.push_back(vec1);
+//		}
+//		Matrix* mulmat = new Matrix(vec);
+//		return mulmat;
+//	}
+//
+//
+//	void printMatrix() {
+//		for (int i = 0; i < this->mat.size(); i++) {
+//			for (int j = 0; j < this->mat[i].size(); j++) {
+//				cout << this->mat[i][j] << " ";
+//			}
+//			cout << endl;
+//		}
+//	}
+//};
+//
+//int main() {
+//	vector<vector<int>> mat1 = { {1, 1, 1},
+//								 {1, 1, 1} };
+//	vector<vector<int>> mat2 = { {1, 2, 3},
+//							     {2, 3, 4},};
+//	
+//	vector<vector<int>> mat3 = { {1, 2},
+//								 {3, 4},
+//								 {5, 6} };
+//
+//	Matrix* mat = new Matrix(mat1);
+//	Matrix* mattrix = new Matrix(mat2);
+//	Matrix* multiMatrix = new Matrix(mat3);
+//	mat->addMatrix(mat)->scaling(5);
+//	mat->subMatrix(mattrix);
+//	mat->mulMatrix(multiMatrix)->printMatrix();
+//}
